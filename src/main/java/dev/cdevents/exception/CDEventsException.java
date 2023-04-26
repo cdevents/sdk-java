4a7b872c02a0d070902c0c6d19ff7d3e49faf3f3
Jalander Ramagiri <jalander.ramagiri@est.tech>
2023-04-26 09:36:07 +0100

    adding Missing JavadocMethod

--- src/main/java/dev/cdevents/exception/CDEventsException.java
+++ src/main/java/dev/cdevents/exception/CDEventsException.java
@@ -2,6 +2,11 @@ package dev.cdevents.exception;
 
 public class CDEventsException extends  RuntimeException {
 
+
+    /**
+     * Custom CDEventsException to throw.
+     * @param message
+     */
     public CDEventsException(String message) {
         super(message);
     }
