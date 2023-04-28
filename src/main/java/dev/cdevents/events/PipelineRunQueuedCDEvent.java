08aad96ef60d52b72a5a8e0eda423d5cde686b69
Jalander Ramagiri <jalander.ramagiri@est.tech>
2023-04-28 16:14:22 +0100

    changes to Create PipelineRunQueuedCDEvent

---
+++ src/main/java/dev/cdevents/events/PipelineRunQueuedCDEvent.java
@@ -0,0 +1,188 @@
+package dev.cdevents.events;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import dev.cdevents.constants.CDEventConstants;
+import dev.cdevents.models.CDEvent;
+import dev.cdevents.models.PipelineRunQueuedSubject;
+
+import java.net.URI;
+
+public class PipelineRunQueuedCDEvent extends CDEvent {
+
+    private static final String CDEVENT_VERSION = "0.1.0";
+    @JsonProperty(required = true)
+    private PipelineRunQueuedSubject subject;
+
+    /**
+     * Constructor to init CDEvent and set the Subject for {@link PipelineRunQueuedCDEvent}.
+     */
+    public PipelineRunQueuedCDEvent() {
+        initCDEvent(currentCDEventType());
+        setSubject(new PipelineRunQueuedSubject(CDEventConstants.SubjectType.PIPELINERUN));
+    }
+
+    /**
+     * @return subject
+     */
+    public PipelineRunQueuedSubject getSubject() {
+        return subject;
+    }
+
+    /**
+     * @param subject
+     */
+    public void setSubject(PipelineRunQueuedSubject subject) {
+        this.subject = subject;
+    }
+
+    /**
+     * @return the PipelineRunQueuedEvent type
+     */
+    @Override
+    public String currentCDEventType() {
+        return CDEventConstants.CDEventTypes.PipelineRunQueuedEvent.getEventType().concat(CDEVENT_VERSION);
+    }
+
+    /**
+     * @return the pipeline-run-queued-event schema URL
+     */
+    @Override
+    public String schemaURL() {
+        return String.format("https://cdevents.dev/%s/schema/pipeline-run-queued-event", CDEventConstants.CDEVENTS_SPEC_VERSION);
+    }
+
+    /**
+     * @return the pipeline-run-queued-event schema Json
+     */
+    @Override
+    public String eventSchema() {
+        return "{\n" +
+                "  \"$schema\": \"https://json-schema.org/draft/2020-12/schema\",\n" +
+                "  \"$id\": \"https://cdevents.dev/0.1.0/schema/pipeline-run-queued-event\",\n" +
+                "  \"properties\": {\n" +
+                "    \"context\": {\n" +
+                "      \"properties\": {\n" +
+                "        \"version\": {\n" +
+                "          \"type\": \"string\",\n" +
+                "          \"enum\": [\n" +
+                "            \"0.1.0\"\n" +
+                "          ],\n" +
+                "          \"default\": \"0.1.0\"\n" +
+                "        },\n" +
+                "        \"id\": {\n" +
+                "          \"type\": \"string\",\n" +
+                "          \"minLength\": 1\n" +
+                "        },\n" +
+                "        \"source\": {\n" +
+                "          \"type\": \"string\",\n" +
+                "          \"minLength\": 1\n" +
+                "        },\n" +
+                "        \"type\": {\n" +
+                "          \"type\": \"string\",\n" +
+                "          \"minLength\": 1\n" +
+                "        },\n" +
+                "        \"timestamp\": {\n" +
+                "          \"type\": \"string\",\n" +
+                "          \"format\": \"date-time\"\n" +
+                "        }\n" +
+                "      },\n" +
+                "      \"additionalProperties\": false,\n" +
+                "      \"type\": \"object\",\n" +
+                "      \"required\": [\n" +
+                "        \"version\",\n" +
+                "        \"id\",\n" +
+                "        \"source\",\n" +
+                "        \"type\",\n" +
+                "        \"timestamp\"\n" +
+                "      ]\n" +
+                "    },\n" +
+                "    \"subject\": {\n" +
+                "      \"properties\": {\n" +
+                "        \"id\": {\n" +
+                "          \"type\": \"string\",\n" +
+                "          \"minLength\": 1\n" +
+                "        },\n" +
+                "        \"source\": {\n" +
+                "          \"type\": \"string\"\n" +
+                "        },\n" +
+                "        \"type\": {\n" +
+                "          \"type\": \"string\",\n" +
+                "          \"minLength\": 1\n" +
+                "        },\n" +
+                "        \"content\": {\n" +
+                "          \"properties\": {\n" +
+                "            \"pipelineName\": {\n" +
+                "              \"type\": \"string\"\n" +
+                "            },\n" +
+                "            \"url\": {\n" +
+                "              \"type\": \"string\"\n" +
+                "            }\n" +
+                "          },\n" +
+                "          \"additionalProperties\": false,\n" +
+                "          \"type\": \"object\"\n" +
+                "        }\n" +
+                "      },\n" +
+                "      \"additionalProperties\": false,\n" +
+                "      \"type\": \"object\",\n" +
+                "      \"required\": [\n" +
+                "        \"id\",\n" +
+                "        \"type\",\n" +
+                "        \"content\"\n" +
+                "      ]\n" +
+                "    },\n" +
+                "    \"customData\": {\n" +
+                "      \"oneOf\": [\n" +
+                "        {\n" +
+                "          \"type\": \"object\"\n" +
+                "        },\n" +
+                "        {\n" +
+                "          \"type\": \"string\",\n" +
+                "          \"contentEncoding\": \"base64\"\n" +
+                "        }\n" +
+                "      ]\n" +
+                "    },\n" +
+                "    \"customDataContentType\": {\n" +
+                "      \"type\": \"string\"\n" +
+                "    }\n" +
+                "  },\n" +
+                "  \"additionalProperties\": false,\n" +
+                "  \"type\": \"object\",\n" +
+                "  \"required\": [\n" +
+                "    \"context\",\n" +
+                "    \"subject\"\n" +
+                "  ]\n" +
+                "}";
+    }
+
+    /**
+     * @param subjectId
+     * sets the subject Id
+     */
+    public void setSubjectId(String subjectId) {
+        getSubject().setId(subjectId);
+    }
+
+    /**
+     * @param subjectSource
+     * sets the pipeline source
+     */
+    public void setSubjectSource(URI subjectSource) {
+        getSubject().setSource(subjectSource);
+    }
+
+    /**
+     * @param pipelineName
+     * sets the pipeline name
+     */
+    public void setSubjectPipelineName(String pipelineName) {
+        getSubject().getContent().setPipelineName(pipelineName);
+    }
+
+    /**
+     * @param subjectUrl
+     * sets the pipeline URL
+     */
+    public void setSubjectUrl(URI subjectUrl) {
+        getSubject().getContent().setUrl(subjectUrl);
+    }
+}
--- src/test/java/dev/cdevents/CDEventsTest.java
+++ src/test/java/dev/cdevents/CDEventsTest.java
@@ -1,7 +1,9 @@
 package dev.cdevents;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import dev.cdevents.constants.CDEventConstants;
 import dev.cdevents.events.PipelineRunFinishedCDEvent;
+import dev.cdevents.events.PipelineRunQueuedCDEvent;
 import dev.cdevents.exception.CDEventsException;
 import io.cloudevents.CloudEvent;
 import org.junit.jupiter.api.Test;
@@ -11,7 +13,6 @@ import java.nio.charset.StandardCharsets;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CDEventsTest {
 
@@ -22,7 +23,7 @@ public class CDEventsTest {
         cdEvent.setSource(URI.create("http://dev.cdevents"));
 
         cdEvent.setSubjectId("/dev/pipeline/run/subject");
-        cdEvent.setSubjectSource(URI.create("/dev/pipeline/run/subject"));
+        cdEvent.setSubjectSource(URI.create("http://dev/pipeline/run/subject"));
         cdEvent.setSubjectPipelineName("Name-pipeline");
         cdEvent.setSubjectUrl(URI.create("http://dev/pipeline/url"));
         cdEvent.setSubjectErrors("errors to place");
@@ -52,4 +53,66 @@ public class CDEventsTest {
         
         assertThat(exception.getMessage()).isEqualTo(expectedError);
     }
+
+    @Test
+    void createPipelineRunQueuedEventAsCloudEvent() {
+
+        PipelineRunQueuedCDEvent cdEvent =  new PipelineRunQueuedCDEvent();
+        cdEvent.setSource(URI.create("http://dev.cdevents"));
+
+        cdEvent.setSubjectId("/dev/pipeline/run/subject");
+        cdEvent.setSubjectSource(URI.create("http://dev/pipeline/run/subject"));
+        cdEvent.setSubjectPipelineName("test-pipeline-name");
+        cdEvent.setSubjectUrl(URI.create("http://dev/pipeline/url"));
+
+        String cdEventJson = CDEvents.cdEventAsJson(cdEvent);
+
+        CloudEvent ceEvent = CDEvents.cdEventAsCloudEvent(cdEvent);
+
+        String ceDataJson = new String(ceEvent.getData().toBytes(), StandardCharsets.UTF_8);
+
+        assertThat(ceEvent.getType()).isEqualTo(cdEvent.getContext().getType());
+        assertThat(ceEvent.getSource()).isEqualTo(cdEvent.getContext().getSource());
+        assertThat(ceDataJson).isEqualTo(cdEventJson);
+
+    }
+
+    @Test
+    void testInvalidPipelineRunQueuedEventWithNoSubject() {
+        PipelineRunQueuedCDEvent cdEvent =  new PipelineRunQueuedCDEvent();
+        cdEvent.setSource(URI.create("http://dev.cdevents"));
+
+        Exception exception = assertThrows(CDEventsException.class, () -> {
+            CDEvents.cdEventAsCloudEvent(cdEvent);
+        });
+        String expectedError = "CDEvent validation failed against schema URL - " + cdEvent.schemaURL();
+
+        assertThat(exception.getMessage()).isEqualTo(expectedError);
+    }
+
+    @Test
+    void testPipelineRunQueuedEventWithCustomData() throws JsonProcessingException {
+        PipelineRunQueuedCDEvent cdEvent =  new PipelineRunQueuedCDEvent();
+        cdEvent.setSource(URI.create("http://dev.cdevents"));
+
+        cdEvent.setSubjectId("/dev/pipeline/run/subject");
+        cdEvent.setSubjectSource(URI.create("http://dev/pipeline/run/subject"));
+        cdEvent.setSubjectPipelineName("test-pipeline-name");
+        cdEvent.setSubjectUrl(URI.create("http://dev/pipeline/url"));
+
+        String customDataJson ="{\"key1\": \"value1\",\"key2\" : {\"test\": \"customData\" }}";
+        cdEvent.setCustomData(customDataJson);
+
+        String cdEventJson = CDEvents.cdEventAsJson(cdEvent);
+
+        CloudEvent ceEvent = CDEvents.cdEventAsCloudEvent(cdEvent);
+
+        String ceDataJson = new String(ceEvent.getData().toBytes(), StandardCharsets.UTF_8);
+
+        assertThat(ceEvent.getType()).isEqualTo(cdEvent.getContext().getType());
+        assertThat(ceEvent.getSource()).isEqualTo(cdEvent.getContext().getSource());
+        assertThat(ceDataJson).isEqualTo(cdEventJson);
+        assertThat(ceDataJson).isEqualTo(cdEventJson);
+    }
+
 }
