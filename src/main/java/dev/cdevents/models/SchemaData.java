8e837e65ca34cd3e33b07d80103bf9412f60ace6
Jalander Ramagiri <jalander.ramagiri@est.tech>
2023-06-27 12:03:13 +0100

    fix linter issues
    Signed-off-by: Jalander Ramagiri <jalander.ramagiri@est.tech>

--- src/main/java/dev/cdevents/models/SchemaData.java
+++ src/main/java/dev/cdevents/models/SchemaData.java
@@ -21,104 +21,164 @@ public class SchemaData {
 
     private List<ContentObjectField> contentObjectFields;
 
-    public SchemaData(){
-
+    /**
+     * Default constructor.
+     */
+    public SchemaData() {
     }
 
+    /**
+     * @return the subject of an event
+     */
     public String getSubject() {
         return subject;
     }
 
+    /**
+     * @param subject
+     */
     public void setSubject(String subject) {
         this.subject = subject;
     }
 
+    /**
+     * @return the predicate of an event
+     */
     public String getPredicate() {
         return predicate;
     }
 
+    /**
+     * @param predicate
+     */
     public void setPredicate(String predicate) {
         this.predicate = predicate;
     }
 
-    public String getVersion() {
-        return version;
-    }
-
-    public void setVersion(String version) {
-        this.version = version;
-    }
-
-    public String getUpperCaseSubject() {
-        return upperCaseSubject;
-    }
-
-    public void setUpperCaseSubject(String upperCaseSubject) {
-        this.upperCaseSubject = upperCaseSubject;
-    }
-
+    /**
+     * @return the Capitalized Subject of an event
+     */
     public String getCapitalizedSubject() {
         return capitalizedSubject;
     }
 
+    /**
+     * @param capitalizedSubject
+     */
     public void setCapitalizedSubject(String capitalizedSubject) {
         this.capitalizedSubject = capitalizedSubject;
     }
 
+    /**
+     * @return the Capitalized predicate of an event
+     */
     public String getCapitalizedPredicate() {
         return capitalizedPredicate;
     }
 
+    /**
+     * @param capitalizedPredicate
+     */
     public void setCapitalizedPredicate(String capitalizedPredicate) {
         this.capitalizedPredicate = capitalizedPredicate;
     }
 
-    public String getSchemaFileName() {
-        return schemaFileName;
-    }
-
-    public void setSchemaFileName(String schemaFileName) {
-        this.schemaFileName = schemaFileName;
-    }
-
-    public List<ContentField> getContentFields() {
-        return contentFields;
-    }
-
-    public void setContentFields(List<ContentField> contentFields) {
-        this.contentFields = contentFields;
-    }
-
-    public List<ContentObjectField> getContentObjectFields() {
-        return contentObjectFields;
-    }
-
-    public void setContentObjectFields(List<ContentObjectField> contentObjectFields) {
-        this.contentObjectFields = contentObjectFields;
-    }
-
+    /**
+     * @return the objectName of an event
+     */
     public String getObjectName() {
         return objectName;
     }
 
+    /**
+     * @param objectName
+     */
     public void setObjectName(String objectName) {
         this.objectName = objectName;
     }
 
+    /**
+     * @return the Capitalized Object name of an event
+     */
     public String getCapitalizedObjectName() {
         return capitalizedObjectName;
     }
 
+    /**
+     * @param capitalizedObjectName
+     */
     public void setCapitalizedObjectName(String capitalizedObjectName) {
         this.capitalizedObjectName = capitalizedObjectName;
     }
 
-    public SchemaData(String subject, String predicate, String version, String upperCaseSubject, String schemaFileName) {
-        this.subject = subject;
-        this.predicate = predicate;
+    /**
+     * @return the version of an event
+     */
+    public String getVersion() {
+        return version;
+    }
+
+    /**
+     * @param version
+     */
+    public void setVersion(String version) {
         this.version = version;
+    }
+
+    /**
+     * @return the Uppercase Subject of an event
+     */
+    public String getUpperCaseSubject() {
+        return upperCaseSubject;
+    }
+
+    /**
+     * @param upperCaseSubject
+     */
+    public void setUpperCaseSubject(String upperCaseSubject) {
         this.upperCaseSubject = upperCaseSubject;
+    }
+
+    /**
+     * @return the Schema file name of an event
+     */
+    public String getSchemaFileName() {
+        return schemaFileName;
+    }
+
+    /**
+     * @param schemaFileName
+     */
+    public void setSchemaFileName(String schemaFileName) {
         this.schemaFileName = schemaFileName;
+    }
+
+    /**
+     * @return the Content fields of an event
+     */
+    public List<ContentField> getContentFields() {
+        return contentFields;
+    }
+
+    /**
+     * @param contentFields
+     */
+    public void setContentFields(List<ContentField> contentFields) {
+        this.contentFields = contentFields;
+    }
+
+    /**
+     * @return the Content Object fields of an event
+     */
+    public List<ContentObjectField> getContentObjectFields() {
+        return contentObjectFields;
+    }
+
+    /**
+     * @param contentObjectFields
+     */
+    public void setContentObjectFields(List<ContentObjectField> contentObjectFields) {
+        this.contentObjectFields = contentObjectFields;
     }
 
     public static class ContentField {
@@ -126,30 +186,53 @@ public class SchemaData {
         private String capitalizedFieldName;
         private String dataType;
 
+        /**
+         * @return the Field name of an event content
+         */
         public String getFieldName() {
             return fieldName;
         }
 
+        /**
+         * @param fieldName
+         */
         public void setFieldName(String fieldName) {
             this.fieldName = fieldName;
         }
 
+        /**
+         * @return the Capitalized Field name of an event content
+         */
         public String getCapitalizedFieldName() {
             return capitalizedFieldName;
         }
 
+        /**
+         * @param capitalizedFieldName
+         */
         public void setCapitalizedFieldName(String capitalizedFieldName) {
             this.capitalizedFieldName = capitalizedFieldName;
         }
 
+        /**
+         * @return the dataType of an event content
+         */
         public String getDataType() {
             return dataType;
         }
 
+        /**
+         * @param dataType
+         */
         public void setDataType(String dataType) {
             this.dataType = dataType;
         }
 
+        /**
+         * @param fieldName
+         * @param capitalizedFieldName
+         * @param dataType
+         */
         public ContentField(String fieldName, String capitalizedFieldName, String dataType) {
             this.fieldName = fieldName;
             this.capitalizedFieldName = capitalizedFieldName;
@@ -164,46 +247,83 @@ public class SchemaData {
         private String capitalizedObjectName;
         private String dataType;
 
+        /**
+         * @return the Field name of an event contentObject
+         */
         public String getFieldName() {
             return fieldName;
         }
 
+        /**
+         * @param fieldName
+         */
         public void setFieldName(String fieldName) {
             this.fieldName = fieldName;
         }
 
+        /**
+         * @return the Capitalized Field name of an event contentObject
+         */
         public String getCapitalizedFieldName() {
             return capitalizedFieldName;
         }
 
+        /**
+         * @param capitalizedFieldName
+         */
         public void setCapitalizedFieldName(String capitalizedFieldName) {
             this.capitalizedFieldName = capitalizedFieldName;
         }
 
+        /**
+         * @return the Object name of an event contentObject
+         */
         public String getObjectName() {
             return objectName;
         }
 
+        /**
+         * @param objectName
+         */
         public void setObjectName(String objectName) {
             this.objectName = objectName;
         }
 
+        /**
+         * @return the Capitalized Object name of an event contentObject
+         */
         public String getCapitalizedObjectName() {
             return capitalizedObjectName;
         }
 
+        /**
+         * @param capitalizedObjectName
+         */
         public void setCapitalizedObjectName(String capitalizedObjectName) {
             this.capitalizedObjectName = capitalizedObjectName;
         }
 
+        /**
+         * @return the dataType of an event contentObject
+         */
         public String getDataType() {
             return dataType;
         }
 
+        /**
+         * @param dataType
+         */
         public void setDataType(String dataType) {
             this.dataType = dataType;
         }
 
+        /**
+         * @param fieldName
+         * @param capitalizedFieldName
+         * @param objectName
+         * @param capitalizedObjectName
+         * @param dataType
+         */
         public ContentObjectField(String fieldName, String capitalizedFieldName, String objectName, String capitalizedObjectName, String dataType) {
             this.fieldName = fieldName;
             this.capitalizedFieldName = capitalizedFieldName;
