739fb098e5fbafad8b57248a1ecddfed4f366bd0
Jalander Ramagiri <97948659+rjalander@users.noreply.github.com>
2023-10-02 12:26:41 +0100

    Generate events using jsonschema2pojo and mustache template (#58)
    Initial changes to generate events using jsonschema2pojo and mustache template, 
    /src/main/resources/template/event-template.mustache
    
    pom.xml has the plugin to generate jsonschema2pojo and plugin to run the main
    class from CDEventsGenerator to create events using mustache template.
    
    Running ./mvnw verify
    
    Generates models using jsonschema2pojo for each event under - dev.cdevents.models.generated.subject.predicate
    Generates events from mustache template and placed under - dev.cdevents.events.generated
    
    Note:
    
    Generating only 3 events for an initial review and schema files for the same are placed under - /src/main/resources/schema (This will essentially point to spec repo to generate all the events once reviewed)
    Removed current Events(dev.cdevents.events) and Models(dev.cdevents.models) that are manually created earlier.
    
    ---------
    
    Signed-off-by: Jalander Ramagiri <jalander.ramagiri@est.tech>
    Co-authored-by: Lapenta Francesco Davide <37077655+lapentad@users.noreply.github.com>

---
+++ sdk/src/main/java/dev/cdevents/config/CustomObjectMapper.java
@@ -0,0 +1,25 @@
+package dev.cdevents.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import java.text.SimpleDateFormat;
+
+public class CustomObjectMapper extends ObjectMapper {
+
+    /**
+     * @return customized ObjectMapper
+     */
+    public ObjectMapper customConfiguration() {
+        return this
+                .enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING)
+                .enable(DeserializationFeature.READ_ENUMS_USING_TO_STRING)
+                .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
+                .setSerializationInclusion(JsonInclude.Include.NON_NULL)
+                .setDateFormat(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'"))
+                .registerModule(new JavaTimeModule());
+    }
+}
