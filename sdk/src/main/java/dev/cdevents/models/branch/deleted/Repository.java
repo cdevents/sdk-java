f7223b0279515acfbfc3501def00664ca6878f00
Jalander Ramagiri <jalander.ramagiri@est.tech>
2023-12-01 14:47:22 +0000

    changes to upgrade Java SDK with v0.3.0 spec
    update unit testing for new events
    
    add unit testing for CDEventClass generated for each schema file
    
    Signed-off-by: Jalander Ramagiri <jalander.ramagiri@est.tech>

--- sdk/src/main/java/dev/cdevents/models/branch/deleted/Repository.java
+++ sdk/src/main/java/dev/cdevents/models/branch/deleted/Repository.java
@@ -8,16 +8,16 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
-    "id",
-    "source"
+        "id",
+        "source"
 })
 @Generated("jsonschema2pojo")
 public class Repository {
 
     /**
-     * 
+     *
      * (Required)
-     * 
+     *
      */
     @JsonProperty("id")
     private String id;
@@ -25,9 +25,9 @@ public class Repository {
     private String source;
 
     /**
-     * 
+     *
      * (Required)
-     * 
+     *
      */
     @JsonProperty("id")
     public String getId() {
@@ -35,9 +35,9 @@ public class Repository {
     }
 
     /**
-     * 
+     *
      * (Required)
-     * 
+     *
      */
     @JsonProperty("id")
     public void setId(String id) {
@@ -52,6 +52,26 @@ public class Repository {
     @JsonProperty("source")
     public void setSource(String source) {
         this.source = source;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 1;
+        result = ((result* 31)+((this.id == null)? 0 :this.id.hashCode()));
+        result = ((result* 31)+((this.source == null)? 0 :this.source.hashCode()));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if ((other instanceof Repository) == false) {
+            return false;
+        }
+        Repository rhs = ((Repository) other);
+        return (((this.id == rhs.id)||((this.id!= null)&&this.id.equals(rhs.id)))&&((this.source == rhs.source)||((this.source!= null)&&this.source.equals(rhs.source))));
     }
 
 }
--- sdk/src/main/java/dev/cdevents/models/incident/reported/Environment.java
+++ sdk/src/main/java/dev/cdevents/models/incident/reported/Environment.java
@@ -8,16 +8,16 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
-    "id",
-    "source"
+        "id",
+        "source"
 })
 @Generated("jsonschema2pojo")
 public class Environment {
 
     /**
-     * 
+     *
      * (Required)
-     * 
+     *
      */
     @JsonProperty("id")
     private String id;
@@ -25,9 +25,9 @@ public class Environment {
     private String source;
 
     /**
-     * 
+     *
      * (Required)
-     * 
+     *
      */
     @JsonProperty("id")
     public String getId() {
@@ -35,9 +35,9 @@ public class Environment {
     }
 
     /**
-     * 
+     *
      * (Required)
-     * 
+     *
      */
     @JsonProperty("id")
     public void setId(String id) {
@@ -52,6 +52,26 @@ public class Environment {
     @JsonProperty("source")
     public void setSource(String source) {
         this.source = source;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 1;
+        result = ((result* 31)+((this.id == null)? 0 :this.id.hashCode()));
+        result = ((result* 31)+((this.source == null)? 0 :this.source.hashCode()));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if ((other instanceof Environment) == false) {
+            return false;
+        }
+        Environment rhs = ((Environment) other);
+        return (((this.id == rhs.id)||((this.id!= null)&&this.id.equals(rhs.id)))&&((this.source == rhs.source)||((this.source!= null)&&this.source.equals(rhs.source))));
     }
 
 }
