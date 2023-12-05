5d17b750fda57fd4a9d7ad2c5baec2bb4b9e2143
Jalander Ramagiri <jalander.ramagiri@est.tech>
2023-12-05 13:39:19 +0000

    add unit testing for CDEventClass generated for each schema file
    Signed-off-by: Jalander Ramagiri <jalander.ramagiri@est.tech>

--- sdk/src/test/java/dev/cdevents/constants/CDEventTypesTest.java
+++ sdk/src/test/java/dev/cdevents/constants/CDEventTypesTest.java
@@ -1,14 +1,41 @@
 package dev.cdevents.constants;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import org.junit.jupiter.api.Test;
-
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.cdevents.constants.CDEventConstants.CDEventTypes;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CDEventTypesTest {
 	@Test
 	void eventTypeNamesAreUnique() {
 		assertThat(CDEventTypes.values()).extracting(CDEventTypes::getEventType).doesNotHaveDuplicates();
+	}
+
+	@Test
+	void eventTypesMatchesWithSchemaContextTypes() throws IOException {
+		ObjectMapper objectMapper = new ObjectMapper();
+		File folder = new File(CDEventConstants.SCHEMA_FOLDER);
+		List<String> eventTypeList = new ArrayList<>();
+		if (folder.isDirectory()) {
+			File[] files = folder.listFiles((dir, name) -> name.toLowerCase().endsWith(".json"));
+			if (files != null) {
+				for (File file : files) {
+					JsonNode rootNode = objectMapper.readTree(file);
+					JsonNode contextNode = rootNode.get("properties").get("context").get("properties");
+					String eventType = contextNode.get("type").get("enum").get(0).asText();
+					eventTypeList.add(eventType.split("\\d+\\.\\d+\\.\\d+")[0]);
+				}
+			}
+		}
+		assertThat(CDEventTypes.values()).extracting(CDEventTypes::getEventType).hasSameElementsAs(eventTypeList);
 	}
 }
