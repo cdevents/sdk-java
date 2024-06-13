6407e771846e8f011b04affb59bccf9dd6ae0397
Jalander Ramagiri <jalander.ramagiri@est.tech>
2024-06-13 12:59:51 +0100

    Fix PreprocessSchemas linting issues
    Signed-off-by: Jalander Ramagiri <jalander.ramagiri@est.tech>

--- preprocessor/src/main/java/dev/cdevents/PreprocessSchemas.java
+++ preprocessor/src/main/java/dev/cdevents/PreprocessSchemas.java
@@ -1,30 +1,18 @@
 package dev.cdevents;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import dev.cdevents.process.ProcessSchemas;
 import dev.cdevents.process.ProcessSchemasImpl;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Iterator;
 
 /**
- * Adapts incoming schemas to a format that jsonschema2pojo
+ * Adapts incoming schemas to a format that jsonschema2pojo.
  */
 public final class PreprocessSchemas {
 
     private PreprocessSchemas() {
     }
     /**
-     *
      * Main method to update schema files.
-     * @param args [0] - spec schemas directory for the cdevents-sdk-java
+     * @param args [0] - spec schemas directory for the cdevents-sdk-java.
      */
     public static void main(String[] args) {
         if (args == null || args.length != 1) {
